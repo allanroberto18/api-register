b634ba2a121439a865b9fb2be5c2df47e0a54823
allan roberto <allanroberto18@gmail.com>
2019-07-19 12:19:56 +0200

    first commit

---
+++ src/main/java/com/alr/apiregister/ApiRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.alr.apiregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegisterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiRegisterApplication.class, args);
+  }
+
+}
