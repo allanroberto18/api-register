50164106d57624537cd3fee742e025003f6470e7
allan roberto <allanroberto18@gmail.com>
2019-07-19 16:00:28 +0200

    include missin @Column(s) for lastName and email on Customer Model

--- src/main/java/com/alr/apiregister/Domain/Models/Customer.java
+++ src/main/java/com/alr/apiregister/Domain/Models/Customer.java
@@ -1,7 +1,6 @@
 package com.alr.apiregister.Domain.Models;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
@@ -25,10 +24,12 @@ public class Customer {
   @Size(min = 2, max = 125, message = "Name must be between 2 and 125 characters long")
   private String fistName;
 
+  @Column(name = "lastName", length = 125, nullable = false)
   @NotEmpty(message = "Last name may not be empty")
   @Size(min = 2, max = 125, message = "Name must be between 2 and 125 characters long")
   private String lastName;
 
+  @Column(name = "email", length = 125, nullable = false)
   @Email(message = "Email invalid")
   @NotEmpty(message = "Email may not be empty")
   private String email;
